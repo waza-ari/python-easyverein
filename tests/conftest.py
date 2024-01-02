8574acba86814d6aa44d0a9700d73d85a3c9433c
Daniel Herrmann <daniel.herrmann1@gmail.com>
2024-01-02 23:07:23 +0100

    fix: add missing fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,9 +5,10 @@ import string
 import pytest
 
 from easyverein import EasyvereinAPI
+from easyverein.models import CustomFieldCreate
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def ev_connection():
     api_url = os.getenv("EV_API_URL", "https://hexa.easyverein.com/api/")
     api_version = os.getenv("EV_API_VERSION", "v1.7")
@@ -21,3 +22,17 @@ def random_string():
     return "".join(
         random.choice(string.ascii_uppercase + string.digits) for _ in range(16)
     )
+
+
+@pytest.fixture(scope="module")
+def example_member(ev_connection):
+    return ev_connection.member.get()[1]
+
+
+@pytest.fixture(scope="module")
+def example_custom_field(ev_connection):
+    custom_field = ev_connection.custom_field.create(
+        CustomFieldCreate(name="Test-Field", kind="e", settings_type="t")
+    )
+    yield custom_field
+    ev_connection.custom_field.delete(custom_field)
