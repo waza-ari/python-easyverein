0b6538a28eef1088bd9741267b395ac95e657fe9
Daniel Herrmann <daniel.herrmann1@gmail.com>
2024-01-02 20:11:05 +0100

    fix: add missing custom_field imports into init file

--- easyverein/models/__init__.py
+++ easyverein/models/__init__.py
@@ -1,9 +1,11 @@
 from .contact_details import ContactDetails, ContactDetailsCreate, ContactDetailsUpdate
+from .custom_field import CustomField, CustomFieldCreate, CustomFieldUpdate
 from .invoice import Invoice, InvoiceCreate, InvoiceUpdate
 from .invoice_item import InvoiceItem, InvoiceItemCreate, InvoiceItemUpdate
 from .member import Member, MemberCreate, MemberUpdate
 
 ContactDetails.model_rebuild()
+CustomField.model_rebuild()
 Invoice.model_rebuild()
 InvoiceItem.model_rebuild()
 Member.model_rebuild()
