08836834631b6693fc7e1ecd8cb510365d6f7a6e
Daniel Herrmann <daniel.herrmann1@gmail.com>
2024-01-16 11:27:01 +0100

    release: Release version 0.2.3

--- easyverein/__init__.py
+++ easyverein/__init__.py
@@ -2,7 +2,7 @@
 Middleware for FastAPI that supports authenticating users against Keycloak
 """
 
-__version__ = "0.2.2"
+__version__ = "0.2.3"
 
 # Export EasyVerein API directly
 from .api import EasyvereinAPI  # noqa: F401
