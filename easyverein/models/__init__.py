938e98610c7953b8c543f2905a1f05458ee9e7fc
Daniel Herrmann <daniel.herrmann1@gmail.com>
2024-01-21 12:10:07 +0100

    fix: export filter Schemas from model subpackage

--- easyverein/models/__init__.py
+++ easyverein/models/__init__.py
@@ -1,13 +1,29 @@
 # noqa: F401
-from .contact_details import ContactDetails, ContactDetailsCreate, ContactDetailsUpdate
-from .custom_field import CustomField, CustomFieldCreate, CustomFieldUpdate
-from .invoice import Invoice, InvoiceCreate, InvoiceUpdate
-from .invoice_item import InvoiceItem, InvoiceItemCreate, InvoiceItemUpdate
-from .member import Member, MemberCreate, MemberUpdate
+from .contact_details import (
+    ContactDetails,
+    ContactDetailsCreate,
+    ContactDetailsUpdate,
+    ContactDetailsFilter,
+)
+from .custom_field import (
+    CustomField,
+    CustomFieldCreate,
+    CustomFieldUpdate,
+    CustomFieldFilter,
+)
+from .invoice import Invoice, InvoiceCreate, InvoiceUpdate, InvoiceFilter
+from .invoice_item import (
+    InvoiceItem,
+    InvoiceItemCreate,
+    InvoiceItemUpdate,
+    InvoiceItemFilter,
+)
+from .member import Member, MemberCreate, MemberUpdate, MemberFilter
 from .member_custom_field import (
     MemberCustomField,
     MemberCustomFieldCreate,
     MemberCustomFieldUpdate,
+    MemberCustomFieldFilter,
 )
 
 ContactDetails.model_rebuild()
