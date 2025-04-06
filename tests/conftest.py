e05bc68ffe568bab23d1f58618486245ef869b74
Daniel Herrmann <daniel.herrmann1@gmail.com>
2025-04-06 08:50:32 +0200

    test: switch tests to v2.0 as v1.7 is not consistent with the documentation

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,7 +10,7 @@ from easyverein.models import CustomFieldCreate
 @pytest.fixture(scope="session")
 def ev_connection():
     api_url = os.getenv("EV_API_URL", "https://easyverein.com/api/")
-    api_version = os.getenv("EV_API_VERSION", "v1.7")
+    api_version = os.getenv("EV_API_VERSION", "v2.0")
     api_key = os.getenv("EV_API_KEY", "")
 
     return EasyvereinAPI(api_key, base_url=api_url, api_version=api_version)
