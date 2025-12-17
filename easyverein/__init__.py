2fca4b884ff14c607979cd19d163b5645c108d6e
Daniel Herrmann <daniel.herrmann1@gmail.com>
2025-12-17 13:54:59 +0100

    build: release version 2.1.2

--- easyverein/__init__.py
+++ easyverein/__init__.py
@@ -2,7 +2,7 @@
 Middleware for FastAPI that supports authenticating users against Keycloak
 """
 
-__version__ = "2.1.1"
+__version__ = "2.1.2"
 
 # Export EasyVerein API directly
 from .api import EasyvereinAPI  # noqa: F401
